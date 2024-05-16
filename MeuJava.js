5b1194d7d9a9ef67ffd9fdc05e760116c84dd013
LucasHenrique26122005 <lucashenrique.unip.webresponsiva@gmail.com>
2024-05-16 11:36:57 -0300

    Add files via upload

---
+++ MeuJava.js
@@ -0,0 +1,19 @@
+
+function callLink1(){
+    window.location = "page1.html"
+    
+}
+
+function callLink2(){
+    window.location = "page2.html"
+    
+}
+
+function callLink3(){
+    window.location = "page3.html"
+    
+}
+function callLink4(){
+    window.location = "index.html"
+    
+}
